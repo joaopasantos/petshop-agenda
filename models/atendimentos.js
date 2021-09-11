a1fa3d8b93009bb0d8106ee8ad347e7a33f926b5
João Albuquerque <joaopasantos0712@gmail.com>
2021-09-11 20:11:41 -0300

    Ajusta colunas restantes da tabela Atendimentos

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require('moment')
 const connection = require('../infrastructure/connection')
 
 class Atendimento {
     add(atendimento){
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:SS')
+        const created_at = moment().format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, data, created_at}
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        connection.query(sql,atendimento, 
+        connection.query(sql,atendimentoDatado, 
                 (err,res)=> {
                     if(err){
                         console.log(err)
