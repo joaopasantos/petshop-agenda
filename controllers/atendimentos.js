49b87a24b94639c617d9128310d79051bb4feaee
João Albuquerque <joaopasantos0712@gmail.com>
2021-09-11 14:17:48 -0300

    Implementa a conexão com o database

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,10 @@
 module.exports = app => {
     app.get('/atendimentos', 
         (req, res) => {res.send('Você está na rota /atendimentos.')})
+
+    app.post('/atendimentos',
+        (req,res)=> {
+            console.log(req.body)
+            res.send('Você está na rota /atendimentos realizando um POST.')
+        })
 }
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const customExpress = require('./config/customExpress')
+const connection = require('./infrastructure/connection')
 
-const app = customExpress()
-app.listen(3000,
-    () => console.log('Server is running on port 3000.'))
+connection.connect((err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log('Successfully connected to the database!')
+    const app = customExpress()
+    app.listen(3000,
+        () => console.log('Server is running on port 3000.'))
+})
