4ea764e4a78658e76a65ff3d7dfb3f54fae7d93c
João Albuquerque <joaopasantos0712@gmail.com>
2021-09-11 21:10:06 -0300

    Implementa validações para as adições à tabela

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,16 +5,38 @@ class Atendimento {
     add(atendimento, res){
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
         const created_at = moment().format('YYYY-MM-DD HH:mm:ss')
-        const atendimentoDatado = {...atendimento, data, created_at}
-        const sql = 'INSERT INTO atendimentos SET ?'
 
-        connection.query(sql,atendimentoDatado, 
-                (err,result)=> {
-                    if(err){
-                        res.status(400).json(err)
-                    }
-                    res.status(201).json(result)
-                })
+        const clienteValido = atendimento.cliente.length >= 3
+        const dataValida = moment(data).isSameOrAfter(created_at)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Valor do campo Data é inválido.'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Campo Cliente deve ter três ou mais caracteres.'
+            },
+        ]
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+            const atendimentoDatado = {...atendimento, data, created_at}
+            const sql = 'INSERT INTO atendimentos SET ?'
+
+            connection.query(sql,atendimentoDatado, 
+                    (err,result)=> {
+                        if(err){
+                            res.status(400).json(err)
+                        }
+                        res.status(201).json(result)
+                    })
+        }
     }
 }
 
